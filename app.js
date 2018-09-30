f54e7fc4bb72a251f115ea174fb11169285319d5
Sukjune Hong <sukjune.hong@gmail.com>
2018-09-30 02:43:28 -0400

    Pushing files for the first time

---
+++ app.js
@@ -0,0 +1,55 @@
+const showHome = function () {
+    $('#homePage').removeClass('hide');
+    $('#viewPage').addClass('hide');
+    $('#verifyPage').addClass('hide');
+    $('#updatePage').addClass('hide');
+    $('#deletePage').addClass('hide');
+}
+const showView = function () {
+    $('#homePage').addClass('hide');
+    $('#viewPage').removeClass('hide');
+    $('#addPage').addClass('hide');
+    $('#verifyPage').addClass('hide');
+    $('#updatePage').addClass('hide');
+    $('#deletePage').addClass('hide');
+}
+const showAdd = function () {
+    $('#homePage').addClass('hide');
+    $('#viewPage').addClass('hide');
+    $('#addPage').removeClass('hide');
+    $('#verifyPage').addClass('hide');
+    $('#updatePage').addClass('hide');
+    $('#deletePage').addClass('hide');
+}
+const showVerify = function () {
+    $('#homePage').addClass('hide');
+    $('#viewPage').addClass('hide');
+    $('#addPage').addClass('hide');
+    $('#verifyPage').removeClass('hide');
+    $('#updatePage').addClass('hide');
+    $('#deletePage').addClass('hide');
+}
+const showUpdate = function () {
+    $('#homePage').addClass('hide');
+    $('#viewPage').addClass('hide');
+    $('#addPage').addClass('hide');
+    $('#verifyPage').addClass('hide');
+    $('#updatePage').removeClass('hide');
+    $('#deletePage').addClass('hide');
+}
+const showDelete = function () {
+    $('#homePage').addClass('hide');
+    $('#viewPage').addClass('hide');
+    $('#addPage').addClass('hide');
+    $('#verifyPage').addClass('hide');
+    $('#updatePage').addClass('hide');
+    $('#deletePage').removeClass('hide');
+}
+$('#home').on('click', showHome);
+$('#view').on('click', showView);
+$('#add').on('click', showAdd);
+$('#verify').on('click', showVerify);
+$('#update').on('click', showUpdate);
+$('#delete').on('click', showDelete);
+
+const 
---
+++ employeeList.js
@@ -0,0 +1,41 @@
+const employeeList = [{
+  firstName: 'Jan',
+  officeNum: 1,
+  phoneNum: '222-222-2222'
+},
+{
+  firstName: 'Juan',
+  officeNum: 304,
+  phoneNum: '489-789-8789'
+},
+{
+  firstName: 'Margie',
+  officeNum: 789,
+  phoneNum: '789-789-7897'
+},
+{
+  firstName: 'Sara',
+  officeNum: 32,
+  phoneNum: '222-789-4654'
+},
+{
+  firstName: 'Tyrell',
+  officeNum: 3,
+  phoneNum: '566-621-0452'
+},
+{
+  firstName: 'Tasha',
+  officeNum: 213,
+  phoneNum: '789-766-5675'
+},
+{
+  firstName: 'Ty',
+  officeNum: 211,
+  phoneNum: '789-766-7865'
+},
+{
+  firstName: 'Sarah',
+  officeNum: 345,
+  phoneNum: '222-789-5231'
+}
+];
